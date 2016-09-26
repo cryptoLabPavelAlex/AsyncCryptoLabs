40ca8483bf1a9c9a14791cf1dad42cef2f3cbf04
AlexeyZatsepin <alexzatsepin7@gmail.com>
2016-09-26 19:14:50 +0300

    Update LehmerHigh.java

--- first/src/main/java/com/pavel/alex/lab/first/generator/LehmerHigh.java
+++ first/src/main/java/com/pavel/alex/lab/first/generator/LehmerHigh.java
@@ -5,12 +5,8 @@ public class LehmerHigh extends AbstractLehmer {
     @Override
     public long generateNext() {
         curState=(a*curState+c) % m;
-        int n = Long.numberOfLeadingZeros(curState) +1;
-        int length = 64 - n;
-//        System.out.println(Long.toBinaryString(curState));
-//        System.out.println(Long.toBinaryString(curState >>> (length - 7) ));
-        return  (length<8) ? curState : curState >>> (length - 7);
-
+        long next = curState>>55;
+        return next;
     }
 
     @Override
